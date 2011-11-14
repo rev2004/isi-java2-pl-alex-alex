9f690c0da7437190a99f1d0df89260e7a1e4990f
laquerre007@gmail.com <laquerre007@gmail.com@ac7ce2a2-5bc3-c447-96cb-2cc46ff186fa>
2011-11-14 16:28:50 +0000

    bla bla bla

---
+++ prjEpicerieJeanguy/src/com/isi/rm/prjepiceriejeanguy/entites/Categorie.java
@@ -0,0 +1,54 @@
+package com.isi.rm.prjepiceriejeanguy.entites;
+
+import com.isi.rm.prjepiceriejeanguy.services.ConnecteurBD;
+
+public class Categorie {
+
+
+/*
+noCategorie           bigint(20)    (NULL)             NO      PRI     (NULL)   auto_increment  select,insert,update,references         
+descriptionCategorie  varchar(100)  latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+image                 varchar(100)  latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+*/
+	
+private int noCategorie;
+String descriptionCategorie;
+String image;
+
+public Categorie(ConnecteurBD cbd, int noCategorie,
+			String descriptionCategorie, String image) {
+		super();
+		this.noCategorie = noCategorie;
+		this.descriptionCategorie = descriptionCategorie;
+		this.image = image;
+	}
+
+
+public int getNoCategorie() {
+	return noCategorie;
+}
+
+public void setNoCategorie(int noCategorie) {
+	this.noCategorie = noCategorie;
+}
+
+public String getDescriptionCategorie() {
+	return descriptionCategorie;
+}
+
+public void setDescriptionCategorie(String descriptionCategorie) {
+	this.descriptionCategorie = descriptionCategorie;
+}
+
+public String getImage() {
+	return image;
+}
+
+public void setImage(String image) {
+	this.image = image;
+}
+
+
+
+
+}
