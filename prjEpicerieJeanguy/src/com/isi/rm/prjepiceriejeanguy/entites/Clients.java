a541a63eeb864f046673e8d3c5e5e49a0e2b9c29
laquerre007 <laquerre007@gmail.com>
2011-11-14 16:28:50 +0000

    bla bla bla

---
+++ prjEpicerieJeanguy/src/com/isi/rm/prjepiceriejeanguy/entites/Clients.java
@@ -0,0 +1,22 @@
+package com.isi.rm.prjepiceriejeanguy.entites;
+
+public class Clients {
+/*
+noClient          bigint(20)    (NULL)             NO      PRI     (NULL)   auto_increment  select,insert,update,references         
+nomClient         varchar(100)  latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+prenomClient      varchar(100)  latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+noCiviqueClient   int(11)       (NULL)             YES             (NULL)                   select,insert,update,references         
+rueClient         varchar(100)  latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+codePostalClient  varchar(10)   latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+villeClient       varchar(100)  latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+noCarteCredit     varchar(30)   latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+telephoneClient   varchar(30)   latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+courrielClient    varchar(100)  latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+passwordClient    varchar(100)  latin1_swedish_ci  YES             (NULL)                   select,insert,update,references         
+*/
+	
+	
+	
+	
+	
+}
